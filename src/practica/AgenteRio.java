559efe6256eccc5f6dff2029cb1a38fa4efd5153
pau795 <pau_795@hotmail.com>
2018-06-14 14:39:28 +0200

    agente rio

--- src/practica/AgenteRio.java
+++ src/practica/AgenteRio.java
@@ -10,20 +10,33 @@ import paqueteSid.impl.DefaultMassaAgua;
 public class AgenteRio extends Agent{
 	
 	private class MyTicker extends TickerBehaviour{
-
-		public MyTicker(Agent a, long period) {
+		
+		ArrayList<MassaAgua> rio;
+		
+		public MyTicker(Agent a, long period, ArrayList<MassaAgua> r) {
+			
 			super(a, period);
+			rio=r;
 		}
 
 		@Override
-		protected void onTick() {
+		protected void onTick() {										//Fluir del rio
+			int l =  rio.size();
+			for (int i = l-2; i>=0; --i) {
+				if(i==0) {
+					rio.set(i+1, rio.get(i));
+					MassaAgua m =AgenteRio.massaAguaPredeterminada();
+					m.addVolumen((float) 100.0);
+					rio.set(i, m);
+				}
+				else rio.set(i+1, rio.get(i));
+			}
+			for (int i=0; i<l; ++i) {
+				System.out.print(rio.get(i).getVolumen().get(0) + " ");		//Print del rio a cada tick
+			}
+			System.out.println();
 		}
-		
-	}
-	
-	
-	
-	
+	}	
 	
 	ArrayList<MassaAgua> rio;
 	int l; //longitud rio;
@@ -38,8 +51,7 @@ public class AgenteRio extends Agent{
 		static private float poxigenob = 1;
 		
 		static public MassaAgua massaAguaPredeterminada() {
-			MassaAgua m= new DefaultMassaAgua();
-			m.addVolumen(pvolumen);
+			MassaAgua m= new DefaultMassaAgua();		//Falta añadir el volumen predeterminado, de momento lo dejo asi para que los tests funcionen
 			m.addTotalNitratos(pnitratos);
 			m.addTotalSulfatos(psulfatos);
 			m.addSolidosSuspension(psolidos);
@@ -49,17 +61,28 @@ public class AgenteRio extends Agent{
 		}
 		
 	//----------------------------------------------
-	
 	protected void setup() {
 		rio = new ArrayList<MassaAgua>();
 		Object[] args = getArguments();
 		if (args.length == 1 ) l = Integer.valueOf((String) args[0]); //la longitud del rio viene determinada por el primer parametro
-		else l = 100; //longitud predeterminada si no hay parametros
-		for (int i=0; i<l; ++i) {
+		else l = 100; //longitud predeterminada si no hay parametros	
+		for (int i=0; i<l; ++i) {						//Test de prueba, para que el rio empiece on masas de volumnes diversos
 			MassaAgua m = massaAguaPredeterminada();
+			m.addVolumen((float)i);
 			rio.add(m);		
 		}
-		
-		
+		MyTicker ticker = new MyTicker(this, 3000, rio);
+		addBehaviour(ticker);
+		for (int i=0; i<l; ++i) {
+			System.out.print(rio.get(i).getVolumen().get(0) + " ");  //Test del rio inicial
+		}
+		System.out.println();
+	}
+	
+	protected void takeDown() {
+		for (int i=0; i<l; ++i) {
+			System.out.print(rio.get(i).getVolumen().get(0) + " ");
+		}
+		System.out.println();
 	}
 }
